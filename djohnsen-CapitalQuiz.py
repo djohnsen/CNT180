ddfc631a419322dc4fa6ec4a9f505867948d73b7
djohnsen <djohnsen@johnsen.org>
2025-08-16 20:06:00 -0700

    Assignment #5 Capital Quiz

---
+++ djohnsen-CapitalQuiz.py
@@ -0,0 +1,76 @@
+# Problem Statement:  "The Capital Quiz Problem".
+# Write a program that creates a dictionary containing the U.S. states as keys, and their capitals as values.
+# (Use the Internet to get a list of the states and their capitals.)
+# The program should then randomly quiz the user by displaying the name of a state and asking the user to enter
+# that state’s capital. The program should keep a count of the number of correct and incorrect responses.
+# (As an alternative to the U.S. states, the program can use the names of countries and their capitals.)
+
+stateCapitals = {
+	"Alabama"	    :	"Montgomery"	    ,
+	"Alaska"	    :	"Juneau"	        ,
+	"Arizona"	    :	"Phoenix"	        ,
+	"Arkansas"  	:	"Little Rock"       ,
+	"California"	:	"Sacramento"	    ,
+	"Colorado"	    :	"Denver"	        ,
+	"Connecticut"	:	"Hartford"	        ,
+	"Delaware"  	:	"Dover"	            ,
+	"Florida"	    :	"Tallahassee"	    ,
+	"Georgia"   	:	"Atlanta"	        ,
+	"Hawaii"    	:	"Honolulu"	        ,
+	"Idaho"	        :	"Boise"	            ,
+	"Illinois"  	:	"Springfield"	    ,
+	"Indiana"	    :	"Indianapolis"	    ,
+	"Iowa"	        :	"Des Moines"        ,
+	"Kansas"	    :	"Topeka"	        ,
+	"Kentucky"  	:	"Frankfort"	        ,
+	"Louisiana" 	:	"Baton Rouge"       ,
+	"Maine"	        :	"Augusta"	        ,
+	"Maryland"  	:	"Annapolis"	        ,
+	"Massachusetts"	:	"Boston"	        ,
+	"Michigan"  	:	"Lansing"	        ,
+	"Minnesota"	    :	"Saint Paul"        ,
+	"Mississippi"	:	"Jackson"	        ,
+	"Missouri"	    :	"Jefferson City"    ,
+	"Montana"	    :	"Helena"	        ,
+	"Nebraska"  	:	"Lincoln"	        ,
+	"Nevada"	    :	"Carson City"       ,
+	"New Hampshire" :	"Concord"           ,
+	"New Jersey"    :   "Trenton"           ,
+	"New Mexico"    :   "Santa Fe"          ,
+	"New York"      :   "Albany"            ,
+	"North Carolina":	"Raleigh"           ,
+	"North Dakota"  :	"Bismarck"          ,
+	"Ohio"	        :	"Columbus"	        ,
+	"Oklahoma"  	:	"Oklahoma City"     ,
+	"Oregon"	    :	"Salem"     	    ,
+	"Pennsylvania"	:	"Harrisburg"	    ,
+	"Rhode Island"  :   "Providence"        ,
+    "South Carolina":   "Columbia"          ,
+	"South Dakota"  :   "Pierre"            ,
+	"Tennessee"	    :	"Nashville"	        ,
+	"Texas"	        :	"Austin"	        ,
+	"Utah"	        :	"Salt Lake City"    ,
+	"Vermont"	    :	"Montpelier"	    ,
+	"Virginia"	    :	"Richmond"	        ,
+	"Washington"	:	"Olympia"	        ,
+	"West Virginia" :   "Charleston"        ,
+	"Wisconsin"	    :	"Madison"	        ,
+	"Wyoming"	    :	"Cheyenne"
+}
+
+countCorrect = 0
+countIncorrect = 0
+
+import random
+while True:
+    (state, capital) = random.choice(list(stateCapitals.items()))
+    print ("What is the capital of the state", state, ":")
+    answer = input()
+    if answer == capital:
+        print("Correct")
+        countCorrect += 1
+    else:
+        print("Incorrect - the capital is", capital)
+        countIncorrect += 1
+
+    print("Score is:", countCorrect, "correct and", countIncorrect, "incorrect.")
