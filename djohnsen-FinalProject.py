0460d9fda4160d9ec29972aa5f1396d605b4102b
djohnsen <djohnsen@johnsen.org>
2025-08-20 21:31:18 -0700

    Assignment #6 Final Project

---
+++ djohnsen-FinalProject.py
@@ -0,0 +1,35 @@
+# Rubrics for grading:
+#
+# 1. You should use Python Functions in your project (20%)
+# 2. You should use Loops (10%)
+# 3. You should use IF-ELSE block (20%)
+# 4. Your source code should be running without errors (30%)
+# 5. You are submitting your own source code and within due date (20%). Please submit individually for grading.
+#
+#
+import os
+
+# use Python Functions in your project
+def myFunction():
+    #do things
+    return
+
+# use Loops
+for filename in os.listdir("."):
+    # Iterate over the files
+    # Use the function I created above to do it
+    continue
+
+# use IF-ELSE block
+if True:
+    # do the true thing
+    myFunction()
+else:
+    # do a different thing
+    myFunction()
+
+
+
+
+
+
